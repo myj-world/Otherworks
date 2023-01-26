f8bbe3a3c14d4089232b206faea2ec918043912e
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2023-01-26 21:04:47 +0300

    Fixed dark view

--- SchoolApp/PISJES/PISJES/app/src/main/java/com/yousufjamil/pisjes/MainActivity.kt
+++ SchoolApp/PISJES/PISJES/app/src/main/java/com/yousufjamil/pisjes/MainActivity.kt
@@ -2,19 +2,26 @@ package com.yousufjamil.pisjes
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -53,6 +60,33 @@ class MainActivity : AppCompatActivity() {
             }
             drawerLayout.closeDrawers()
             true
+        }
+
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+//        when (currentNightMode) {
+//            Configuration.UI_MODE_NIGHT_NO -> {} // Night mode is not active, we're using the light theme
+//            Configuration.UI_MODE_NIGHT_YES -> {} // Night mode is active, we're using dark theme
+//        }
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_NO) {
+            val logonav = navView.getHeaderView(0).findViewById<ImageView>(R.id.logonav)
+            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.logotwo)
+            val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+            rounded.isCircular = true
+            logonav.setImageDrawable(rounded)
+        } else {
+            val navheader = navView.getHeaderView(0)
+            val name = navheader.findViewById<TextView>(R.id.name)
+            val urlnav = navheader.findViewById<TextView>(R.id.url)
+            val logonav = navView.getHeaderView(0).findViewById<ImageView>(R.id.logonav)
+            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.logo)
+            val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+            rounded.isCircular = true
+            logonav.setImageDrawable(rounded)
+            val navnewbg = ColorDrawable(Color.parseColor("#068a45"))
+            val navtextcolour = Color.parseColor("#FFFFFF")
+            navheader.background = navnewbg
+            name.setTextColor(navtextcolour)
+            urlnav.setTextColor(navtextcolour)
         }
 
 
