abe16449770b2fa08ee3c99ea0d5daac8144eedf
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2023-10-07 15:04:02 +0300

    Updates

--- Accorm/App/Accorm/app/build.gradle.kts
+++ Accorm/App/Accorm/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -67,5 +67,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 }
--- Accorm/App/Accorm/app/src/main/java/com/yousufjamil/accorm/MainActivity.kt
+++ Accorm/App/Accorm/app/src/main/java/com/yousufjamil/accorm/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,13 +22,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -43,6 +49,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -71,6 +78,7 @@ import kotlinx.coroutines.launch
 
 lateinit var navController: NavHostController
 lateinit var poppins: FontFamily
+lateinit var lexend: FontFamily
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
@@ -85,8 +93,15 @@ class MainActivity : ComponentActivity() {
             Font(R.font.poppins_regular, FontWeight.Normal)
         )
 
+        lexend = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_bold, FontWeight.Bold)
+        )
+
         setContent {
             AccormTheme {
+
                 navController = rememberNavController()
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
                 val scope = rememberCoroutineScope()
@@ -95,12 +110,8 @@ class MainActivity : ComponentActivity() {
                         drawerState = drawerState,
                         drawerContent = {
                             ModalDrawerSheet {
-                                Box(
-                                    modifier = Modifier
-                                        .fillMaxSize(),
-                                    contentAlignment = Alignment.Center
-                                ) {
-                                    Text(text = "Drawer")
+                                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                                    NavigationDrawer() { scope.launch { drawerState.close() } }
                                 }
                             }
                         }
@@ -115,7 +126,9 @@ class MainActivity : ComponentActivity() {
                                                 Text(
                                                     text = "Accorm",
                                                     color = Color.White,
-                                                    modifier = Modifier.fillMaxWidth(0.95f).padding(top = 6.dp),
+                                                    modifier = Modifier
+                                                        .fillMaxWidth(0.95f)
+                                                        .padding(top = 6.dp),
                                                     textAlign = TextAlign.Left,
                                                     fontFamily = poppins,
                                                     fontWeight = FontWeight.SemiBold
@@ -169,6 +182,209 @@ fun Navigation(context: Context, navHostController: NavHostController) {
 }
 
 @Composable
+fun NavigationDrawer(closeDrawer: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(46, 55, 72, 255))
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        @Composable
+        fun Divider() {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .background(Color(33, 41, 56, 255))
+                    .height(2.dp)
+            )
+        }
+//        Row (
+//            horizontalArrangement = Arrangement.End,
+//            modifier = Modifier.fillMaxWidth()
+//        ) {
+//            IconButton(onClick = { closeDrawer() }) {
+//                Icon(imageVector = Icons.Default.Close, contentDescription = "Close Drawer", tint = Color.White)
+//            }
+//        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.app_ic),
+                contentDescription = "App icon",
+                modifier = Modifier
+                    .size(70.dp)
+                    .clip(RoundedCornerShape(corner = CornerSize(50.dp)))
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = "Accorm",
+                fontFamily = lexend,
+                color = Color.White,
+                fontSize = 35.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(1.dp)),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.book_open),
+                        contentDescription = "Subjects",
+                        tint = Color(171, 171, 248, 255),
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Subjects",
+                        fontFamily = lexend,
+                        fontSize = 19.sp
+                    )
+                }
+            }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(1.dp)),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.apps),
+                        contentDescription = "Apps",
+                        tint = Color(171, 171, 248, 255),
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Features",
+                        fontFamily = lexend,
+                        fontSize = 19.sp
+                    )
+                }
+            }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(1.dp)),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "Info",
+                        tint = Color(171, 171, 248, 255),
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Info",
+                        fontFamily = lexend,
+                        fontSize = 19.sp
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(1.dp)),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AddCircle,
+                        contentDescription = "Contribute",
+                        tint = Color(171, 171, 248, 255),
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Contribute",
+                        fontFamily = lexend,
+                        fontSize = 19.sp
+                    )
+                }
+            }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(1.dp)),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Profile",
+                        tint = Color(171, 171, 248, 255),
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Profile",
+                        fontFamily = lexend,
+                        fontSize = 19.sp
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(20.dp))
+        IconButton(
+            onClick = { },
+            colors = IconButtonDefaults.filledIconButtonColors(
+                containerColor = Color(172, 172, 249, 255)
+            ),
+            modifier = Modifier.clip(RoundedCornerShape(corner = CornerSize(100.dp))).size(50.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.wb_sunny),
+                contentDescription = "Change Theme",
+                tint = Color(33, 33, 43, 255),
+                modifier = Modifier.padding(10.dp).size(100.dp)
+            )
+        }
+    }
+}
+
+@Composable
 fun HomeScreen(context: Context) {
     Column(
         modifier = Modifier
@@ -178,44 +394,45 @@ fun HomeScreen(context: Context) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(100.dp))
-        Text(text = buildAnnotatedString {
-            withStyle(
-                SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
-            ) {
-                append("Educate")
+        Text(
+            text = buildAnnotatedString {
                 withStyle(
-                    SpanStyle(
-                        color = Color(144, 144, 214, 255)
-                    )
+                    SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
                 ) {
-                    append(".\n\n\n")
+                    append("Educate")
+                    withStyle(
+                        SpanStyle(
+                            color = Color(144, 144, 214, 255)
+                        )
+                    ) {
+                        append(".\n\n\n")
+                    }
                 }
-            }
-            withStyle(
-                SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
-            ) {
-                append("Empower")
                 withStyle(
-                    SpanStyle(
-                        color = Color(144, 144, 214, 255)
-                    )
+                    SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
                 ) {
-                    append(".\n\n\n")
+                    append("Empower")
+                    withStyle(
+                        SpanStyle(
+                            color = Color(144, 144, 214, 255)
+                        )
+                    ) {
+                        append(".\n\n\n")
+                    }
                 }
-            }
-            withStyle(
-                SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
-            ) {
-                append("Excel")
                 withStyle(
-                    SpanStyle(
-                        color = Color(144, 144, 214, 255)
-                    )
+                    SpanStyle(color = Color.White, fontFamily = poppins, fontSize = 60.sp)
                 ) {
-                    append(".\n\n")
+                    append("Excel")
+                    withStyle(
+                        SpanStyle(
+                            color = Color(144, 144, 214, 255)
+                        )
+                    ) {
+                        append(".\n\n")
+                    }
                 }
-            }
-        },
+            },
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center
         )
